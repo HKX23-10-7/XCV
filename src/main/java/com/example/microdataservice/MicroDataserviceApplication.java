782d8ddf4bc756962c23b9ac3a76286f011e7a4f
HKX <a29851322692023@163.com>
2023-10-07 10:01:12 +0800

    a

---
+++ src/main/java/com/example/microdataservice/MicroDataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroDataserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroDataserviceApplication.class, args);
+    }
+
+}
